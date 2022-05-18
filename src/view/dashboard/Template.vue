ddcb781c299fad0a8bfeb453d878291b30c62521
Julia Moura <juliasilvamoura100@gmail.com>
2022-05-18 08:31:54 -0300

    config basica Navbar e Sidebar

---
+++ src/view/dashboard/Template.vue
@@ -0,0 +1,20 @@
+// importar NAV e SIDE
+<template>
+  
+</template>
+
+<script>
+import Navbar from './../dashboard/navbar/Navbar.vue'
+import Sidebar from './../dashboard/sidebar/Sidebar.vue'
+export default {
+    components:{
+        Navbar,
+        Sidebar
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/view/dashboard/sidebar/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="navbar">
+        <div class="geral">
+            <label>Geral</label>
+            <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/">Inventário</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/">Sair</router-link>
+        </li>
+
+        <label>Colaboradores</label>
+            <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/">Cadastrar</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/">Listar</router-link>
+        </li>
+
+        <label>Produtos</label>
+            <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/">Cadastrar</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/">Empréstimo</router-link>
+        </li>
+            
+        </div>
+        <!-- <div class="icons">
+            <btn type="primary" :full="false" icon="fa-user">Fazer Login</btn>
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
