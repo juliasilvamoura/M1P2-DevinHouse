653486be72657ed31a18652ca4f8011b0357e849
Julia Moura <juliasilvamoura100@gmail.com>
2022-05-27 21:43:21 -0300

    final componentes

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './store'
 import VueGravatar from "vue3-gravatar";
+<<<<<<< Updated upstream
+=======
+import MDialogPlugin from 'vue-m-dialog'
+import 'vue-m-dialog/dist/style.css'
+>>>>>>> Stashed changes
 
 import router from './router'
 const app = createApp(App)
@@ -9,4 +14,8 @@ const app = createApp(App)
 app.use(router)
 app.use(store)
 app.use(VueGravatar);
+<<<<<<< Updated upstream
+=======
+app.use(MDialogPlugin)
+>>>>>>> Stashed changes
 app.mount('#app')
