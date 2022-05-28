9b5f7d469fdb65ba0301f6b42a03989580f82ead
Julia Moura <juliasilvamoura100@gmail.com>
2022-05-27 21:43:53 -0300

    final

--- src/view/dashboard/Template.vue
+++ src/view/dashboard/Template.vue
@@ -1,13 +1,25 @@
+<<<<<<< Updated upstream
 <template>
 <div class="template">
     <div style="position: relative" v-if="autenticado">
         <Navbar/>
         <Sidebar/>
         <router-view style="position: relative; left: 10%"></router-view>
+=======
+<template class="template">
+<div>
+    <div v-if="autenticado">
+        <Navbar class="nav" />
+        <Sidebar class="side"/>
+        <div class="tela">
+        <router-view></router-view>
+        </div>
+>>>>>>> Stashed changes
     </div>
 
     <div class="login-template" v-else>
         <label> Ações de login são necessárias -</label>
+<<<<<<< Updated upstream
          <router-link to="/login"> Login</router-link>
     </div>
  
@@ -16,6 +28,12 @@
 </div>
 
   
+=======
+         <router-link to="/"> Login</router-link>
+    </div>
+
+</div>
+>>>>>>> Stashed changes
 </template>
 
 <script>
@@ -36,12 +54,21 @@ export default {
     },
     computed:{
         autenticado(){
+<<<<<<< Updated upstream
             // return this.$store.state.autenticacaoModule.autenticado;
             return true
         }
     },
     monted(){
         this.$store.satte.autenticacaoModule.autenticado = localStorage.getItem('autenticado') ? true : false
+=======
+            return this.$store.state.userModule.autenticado;
+        },
+        
+    },
+    monted(){
+        this.$store.state.autenticacaoModule.autenticado = localStorage.getItem('autenticado') ? true : false
+>>>>>>> Stashed changes
     }
 
 }
@@ -54,10 +81,36 @@ export default {
     font-size: 100%;
 }
 
+<<<<<<< Updated upstream
 .template{
 
 }
 
+=======
+template{
+    display: grid;
+    grid-template-columns: 1fr 3fr ;
+    grid-template-rows: 20vh 80vh;
+    grid-template-areas: "menuu nav"
+                        "menuu tela";
+}
+
+.nav{
+    grid-area: nav;
+
+}
+
+.side{
+    grid-area: menuu;
+
+}
+
+.tela{
+    grid-area: tela;
+    margin-left: 12%;
+    margin-top: 1%;
+}
+>>>>>>> Stashed changes
 
 
 </style>
